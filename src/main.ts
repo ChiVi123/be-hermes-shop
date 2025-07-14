e0fada6c4f941aa366767660a61154a347b1ba65
ChiVi123 <nguyenhoangchivi@gmail.com>
2025-07-14 16:41:50 +0700

    Alias paths

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '~/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('v1');
   await app.listen(process.env.PORT ?? 8080);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap().catch((err) => {
   console.error('Error during bootstrap:', err);
 });
